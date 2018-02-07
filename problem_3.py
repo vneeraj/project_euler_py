be24c5bb27df39493347d5553d59e9c50ac8196a
vneeraj <v.neeraj@gmail.com>
2018-02-06 23:23:53 -0800

    - Solution to problem 3

---
+++ problem_3.py
@@ -0,0 +1,35 @@
+# Script to solve Problem 3 on Project Euler
+import math
+
+# Returns true if x is prime
+def primality_test(x):
+    if x == 0 or x == 1:
+        return False
+    # Quick checks of 2, 3, 5
+    elif x == 2 or x == 3 or x == 5:
+        return True
+    elif (x % 2 == 0) or (x % 3 == 0) or (x % 5 == 0):
+        return False
+    else:
+        # Loop over potential factors between 7 and sqrt(x)
+        max_candidate = int(math.ceil(math.sqrt(x)))
+        for i in range(7, max_candidate):
+            if (x % i == 0):
+                return False
+        return True
+
+input_val = 600851475143
+max_prime_factor = 0;
+
+# Loop over possible factors up to sqrt(input), 
+# checking if they are prime while keeping track of max
+for i in range(2, math.ceil(math.sqrt(input_val))):
+    if (input_val % i == 0) and primality_test(i):
+        # Since we only go up to sqrt(input), we have to check codivisor as well
+        if(primality_test(input_val/i)):
+            max_prime_factor = max(max_prime_factor, i, input_val/i)
+        else:
+            max_prime_factor = max(max_prime_factor, i)            
+        
+print(max_prime_factor)
+print(primality_test(max_prime_factor))
