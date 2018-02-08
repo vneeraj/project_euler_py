1a63ef23b6283af3fa521954f1bf644458407abd
vneeraj <v.neeraj@gmail.com>
2018-02-07 22:02:54 -0800

    - Minor fixes

--- problem_4.py
+++ problem_4.py
@@ -1,11 +1,10 @@
 # Script to solve Problem 4 on Project Euler
 max_palindrome = 0;
 
-for i in range(100, 999):
-    for j in range(100, 999):
+for i in range(100, 1000):
+    for j in range(100, 1000):
         product = i*j
         if (str(product) == str(product)[::-1]) and product > max_palindrome:
             max_palindrome = product
             
-print(max_palindrome)      
-print(i, j)
+print(max_palindrome)
