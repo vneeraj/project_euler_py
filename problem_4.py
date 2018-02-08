dba2d97e066eea4276c024bf68479bada28948e4
vneeraj <v.neeraj@gmail.com>
2018-02-07 22:01:39 -0800

    - Minor fixes

--- problem_4.py
+++ problem_4.py
@@ -4,8 +4,7 @@ max_palindrome = 0;
 for i in range(100, 999):
     for j in range(100, 999):
         product = i*j
-        product_str = str(product)
-        if (product_str == product_str[::-1]) and product > max_palindrome:
+        if (str(product) == str(product)[::-1]) and product > max_palindrome:
             max_palindrome = product
             
 print(max_palindrome)      
