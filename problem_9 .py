044d54d76e737acb80c45a1ecf58eaab9f05ad55
vneeraj <v.neeraj@gmail.com>
2018-02-12 23:24:49 -0800

    - Solution to problem 9

---
+++ problem_9 .py
@@ -0,0 +1,13 @@
+# Script to solve Problem 9 on Project Euler
+exit_flag = False
+for i in range(1, 999):
+    for j in range(1, 999):
+        k = 1000 - i - j
+        vals = [i, j, k]
+        vals.sort()        
+        if (vals[2]**2 - vals[1]**2) == vals[0]**2:
+            exit_flag = True
+            break    
+    if exit_flag:
+        break
+print(i, j, k, i*j*k)              
