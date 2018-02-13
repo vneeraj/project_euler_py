a531d4fcc3f6a6042b6c356e98a629f068c70975
vneeraj <v.neeraj@gmail.com>
2018-02-12 23:31:44 -0800

    - Minor fix

--- problem_9.py
+++ problem_9.py
@@ -1,7 +1,7 @@
 # Script to solve Problem 9 on Project Euler
 exit_flag = False
 for i in range(1, 999):
-    for j in range(1, 999):
+    for j in range(1, 1000-i-1):
         k = 1000 - i - j
         vals = [i, j, k]
         vals.sort()        
